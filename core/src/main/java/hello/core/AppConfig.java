10eb7c4cc1ffda17131fc64cf2ad986ea42f84df
homoonshi <homoonshi@gmail.com>
2024-09-23 02:28:32 +0900

    Feat : Section 05 스프링 컨테이너와 스프링 빈

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,11 +1,10 @@
 package hello.core;
 
-import hello.core.Member.MemberRepository;
-import hello.core.Member.MemberService;
-import hello.core.Member.MemberServiceImpl;
-import hello.core.Member.MemoryMemberRepository;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,11 +1,8 @@
 package hello.core.order;
 
-import hello.core.Member.Member;
-import hello.core.Member.MemberRepository;
-import hello.core.Member.MemoryMemberRepository;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 
 public class OrderServiceImpl implements OrderService{
 
