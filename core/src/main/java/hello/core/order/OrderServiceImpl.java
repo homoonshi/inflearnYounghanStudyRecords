14dd0621414962b607cda1117089e5346358c46f
homoonshi <homoonshi@gmail.com>
2024-09-26 22:12:45 +0900

    Feat : 스프링 핵심 원리 기본편 -  Section 08 의존관계 자동 주입 - 조회 빈이 2개 이상 - 문제

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,20 +3,41 @@ package hello.core.order;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.discount.DiscountPolicy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
+//@RequiredArgsConstructor // final 붙은 애들을 생성자 만들어줌 -> 생성자 주입
 public class OrderServiceImpl implements OrderService{
 
-  private final MemberRepository memberRepository;
-  private final DiscountPolicy discountPolicy;
+  @Autowired private final MemberRepository memberRepository;
+  @Autowired private final DiscountPolicy discountPolicy;
 
   @Autowired
   public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
   }
+
+//  @Autowired
+//  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//    this.discountPolicy = discountPolicy;
+//  }
+//
+//  @Autowired
+//  public void setMemberRepository(MemberRepository memberRepository) {
+//    this.memberRepository = memberRepository;
+//  }
+
+
+
+//  @Autowired
+//  public void init(MemberRepository memberRepository,DiscountPolicy discountPolicy){
+//    this.memberRepository = memberRepository;
+//    this.discountPolicy = discountPolicy;
+//  }
 
 //  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
@@ -29,9 +50,9 @@ public class OrderServiceImpl implements OrderService{
     return new Order(memberId, itemName, itemPrice, discountPrice);
   }
 
-  // 테스트 용도
-  public MemberRepository getMemberRepository() {
-    return memberRepository;
-  }
+//  // 테스트 용도
+//  public MemberRepository getMemberRepository() {
+//    return memberRepository;
+//  }
 
 }
