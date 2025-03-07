07038b7480587c4754c7dabc2b6158a18f7e3ce5
homoonshi <homoonshi@gmail.com>
2025-03-08 08:34:58 +0900

    Feat : 자바 ORM 표준 JPA 프로그래밍 기본편 - Section 12 객체지향 쿼리 언어 2 - 중급 문법

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -2,6 +2,8 @@ package jpql;
 
 import jakarta.persistence.*;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class JpaMain {
@@ -84,19 +86,19 @@ public class JpaMain {
 //                System.out.println("member1 = " + member1);
 //            }
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
-            member.changeTeam(team);
-            member.setType(MemberType.ADMIN);
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
+//            Member member = new Member();
+//            member.setUsername("관리자");
+//            member.setAge(10);
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
+//            member.changeTeam(team);
+//            member.setType(MemberType.ADMIN);
+//
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
 
 //            String query =
 //                    "select " +
@@ -110,12 +112,12 @@ public class JpaMain {
 //            String query = "select NULLIF(m.username, '관리자') from Member m";
 //            String query = "select concat('a','b') from Member m";
 //            String query = "select locate('de','abcdegf') from Member m";
-            String query = "select size(t.members) from Team t";
-            List<Integer> resultList = em.createQuery(query, Integer.class).getResultList();
-
-            for (Integer s : resultList) {
-                System.out.println("s = " + s);
-            }
+//            String query = "select size(t.members) from Team t";
+//            List<Integer> resultList = em.createQuery(query, Integer.class).getResultList();
+//
+//            for (Integer s : resultList) {
+//                System.out.println("s = " + s);
+//            }
 
 //
 ////            String query = "select m from Member m, Team t where m.username = t.name";
@@ -138,7 +140,132 @@ public class JpaMain {
 //                System.out.println("objects = " + objects[2]);
 //            }
 
+//            Team team = new Team();
+//            em.persist(team);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("관리자1");
+//            member1.changeTeam(team);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("관리자2");
+//            member2.changeTeam(team);
+//            em.persist(member2);
+//
+//            em.flush();
+//            em.clear();
+//
+////            String query = "select m.username From Member m";
+////            String query = "select m.team From Member m";
+////            String query = "select size(t.members) From Team t";
+//            String query = "select t.members From Team t";
+//
+//            Collection result = em.createQuery(query, Collection.class)
+//                    .getResultList();
+//
+//            System.out.println("result = " + result);
+
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.changeTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.changeTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.changeTeam(teamB);
+            em.persist(member3);
+
+//            String query = "select m From Member m";
+//
+//            List<Member> result = em.createQuery(query, Member.class).getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() +", "+ member.getTeam().getName());
+//                // 회원1, 팀A (SQL)
+//                // 회원2, 팀A (1차 캐시)
+//                // 회원3, 팀B (SQL)
+//            }
+
+//            String query = "select m From Member m join fetch m.team";
+//
+//            List<Member> result = em.createQuery(query, Member.class).getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() +", "+ member.getTeam().getName());
+//            }
+
+//            // 컬렉션 페치 조인
+//            String query = "select t From Team t join fetch t.members";
+//
+//            List<Team> result = em.createQuery(query, Team.class).getResultList();
+//
+//            for (Team team : result) {
+//                System.out.println("team = " + team.getName()+", "+team.getMembers().size());
+//                for (Member member : team.getMembers()) {
+//                    System.out.println("member = " + member);
+//                }
+//            }
+
+//            String query = "select t From Team t join t.members m";
+//
+//            List<Team> result = em.createQuery(query, Team.class).getResultList();
+//
+//            for (Team team : result) {
+//                System.out.println("team = " + team.getName()+", "+team.getMembers().size());
+//                for (Member member : team.getMembers()) {
+//                    System.out.println("member = " + member);
+//                }
+//            }
+
+//            String query = "select m From Member m where m = :member";
+//
+//            Member findMember = em.createQuery(query, Member.class)
+//                    .setParameter("member", member1)
+//                    .getSingleResult();
+//
+//            System.out.println("findMember = " + findMember);
+
+//            String query = "select m From Member m where m.team = :team";
+//
+//            List<Member> findMember = em.createQuery(query, Member.class)
+//                    .setParameter("team", teamA)
+//                    .getResultList();
+//
+//            System.out.println("findMember = " + findMember);
+
+//            Member findMember = em.createNamedQuery("Member.findByUsername", Member.class)
+//                    .setParameter("username", "회원1")
+//                    .getSingleResult();
+//
+//            System.out.println("findMember = " + findMember);
+
+            // FLUSH 자동 호출 commit, query, flush
+            int resultCount = em.createQuery("update Member m set m.age = 35")
+                    .executeUpdate();
+
+            System.out.println("resultCount = " + resultCount);
+
+            em.clear();
+
+            Member findMem = em.find(Member.class, member1.getId());
+            System.out.println("findMem.getAge() = " + findMem.getAge());
+
             tx.commit();
+
         } catch (Exception e){
             tx.rollback();
             e.printStackTrace();
